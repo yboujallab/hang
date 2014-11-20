27a534523c56da4e2fb0e8842cefcfe5bd194e18
youssef boujallab <youssef.boujallab&gmail.com>
2014-11-21 00:49:53 +0100

    add of spring mvc

---
+++ src/main/java/com/ma/hang/web/controller/HelloController.java
@@ -0,0 +1,56 @@
+package com.ma.hang.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+ 
+@Controller
+//@RequestMapping(value = "/hello")
+public class HelloController {
+ 
+	@RequestMapping(value = "/welcome" , method = RequestMethod.GET)
+	public ModelAndView welcomePage() {
+ 
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security Hello World");
+		model.addObject("message", "This is welcome page!");
+		model.setViewName("hello");
+		return model;
+ 
+	}
+ 
+	@RequestMapping(value = "/admin**", method = RequestMethod.GET)
+	public ModelAndView adminPage() {
+ 
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security Hello World");
+		model.addObject("message", "This is protected page - Admin Page!");
+		model.setViewName("admin");
+ 
+		return model;
+ 
+	}
+ 
+	@RequestMapping(value = "/dba**", method = RequestMethod.GET)
+	public ModelAndView dbaPage() {
+ 
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security Hello World");
+		model.addObject("message", "This is protected page - Database Page!");
+		model.setViewName("admin");
+ 
+		return model;
+ 
+	}
+	@RequestMapping("/hello")
+	 public String hello(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+	 model.addAttribute("name", name);
+	 return "helloworld";
+	 }
+	
+}
---
+++ src/main/webapp/WEB-INF/pages/admin.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8" session="true"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<h1>Title : ${title}</h1>
+	<h1>Message : ${message}</h1>
+ 
+	<c:if test="${pageContext.request.userPrincipal.name != null}">
+		<h2>Welcome : ${pageContext.request.userPrincipal.name} 
+                 | <a href="<c:url value="/logout" />" > Logout</a></h2>  
+	</c:if>
+</body>
+</html>
